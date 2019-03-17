a084ee4fa0248a467fed166ca6d2ec70f644c8a5
suphakit01 <43027793+suphakit01@users.noreply.github.com>
2019-03-17 17:46:18 +0700

    Add files via upload

--- js.js
+++ js.js
@@ -521,8 +521,6 @@ class Animation {
         var wavet = TweenMax.to('.top_wave', speed*1.7/42, {backgroundPositionX: '-=54px', repeat: -1, ease: Power0.easeNone});
         var wave1 = TweenMax.to('.wave1', speed*8, {backgroundPositionX: '-=1760px', repeat: -1, ease: Power0.easeNone});
         var wave2 = TweenMax.to('.wave2', speed*8, {backgroundPositionX: '-=1760px', repeat: -1, ease: Power0.easeNone});
-        var wave3 = TweenMax.to('.wave3', speed*2.2/42, {backgroundPositionX: '-=54px', repeat: -1, ease: Power0.easeNone});
-        var wave4 = TweenMax.to('.wave4', speed*2.4/42, {backgroundPositionX: '-=54px', repeat: -1, ease: Power0.easeNone});
 
         var mount1 = TweenMax.to('.mount1', speed*8, {backgroundPositionX: '-=1760px', repeat: -1, ease: Power0.easeNone});
         var mount2 = TweenMax.to('.mount2', speed*10, {backgroundPositionX: '-=1782px', repeat: -1, ease: Power0.easeNone});
